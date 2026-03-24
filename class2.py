22f7ee5475749fc53fa97d90225e782bf45e2dcf
Vetri L <vsvetri9@gmail.com>
2026-03-24 01:25:10 -0700

    Practiced python basics

---
+++ class2.py
@@ -0,0 +1,35 @@
+# functions in python
+
+def add(a:int , b:int)->int:
+    return a+b
+
+print(add(4,5))
+
+# SUBTRACT FUNCTION
+
+def sub(a:int,b:int)->int:
+    return(a-b)
+
+print(sub(2,6))
+
+
+#multiplication
+
+def mul(a:int,b:int)->int:
+    return(a*b)
+print(mul(6,8))
+
+
+#Division
+
+def div(a:int,b:int)->int:
+    return(a/6)
+print(div(67,7))
+
+
+
+#Modulus
+
+def mod(a:int,b:int)->int:
+    return(a%b)
+print(mod(10,5))
