d4da8dd55fc72ca7fe91d14fd9e0ea4b4d320925
Vetri L <vsvetri9@gmail.com>
2026-03-24 07:17:09 -0700

    Practiced python fundamentals

---
+++ dt2.py
@@ -0,0 +1,15 @@
+# list , tuple , set, dict and print their dt
+
+employee_names=["vetri","virat","rohit"]
+e_ids=(101,102,103)
+sizes={38}
+
+details={
+    "product_id":101,
+    "employee_name":"vetri"
+}
+
+print(type(employee_names))
+print(type(e_ids))
+print(type(sizes))
+print(type(details))
