5d3e051e30aa0f2f7dc5a465b67e15974c2a6454
Vetri L <vsvetri9@gmail.com>
2026-03-25 07:17:09 -0700

    Practiced python

---
+++ class5.py
@@ -0,0 +1,28 @@
+s={}  #dict type
+
+#to create empty set
+s1=set()
+
+print(type(s))
+print(type(s1))
+
+
+# create
+
+eids={1,2,3,4,4,4}
+
+#Read 
+
+print(eids)  # duplicates are not allowed
+
+e_names={"virat","rohit","dhoni"}  # Unordered
+#Update
+
+e_names.add("vetri")
+print(e_names)
+
+
+#Delete
+e_names.clear()
+print(e_names)
+
