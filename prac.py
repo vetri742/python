08795dee5e7ec0c04e5993e927f19b2a8d0459ca
Vetri L <vsvetri9@gmail.com>
2026-03-26 05:48:55 -0700

    Practiced PYTHON

---
+++ prac.py
@@ -0,0 +1,39 @@
+a = {1, 2, 3}
+
+b = {3, 4, 5}
+
+print(a | b)   # union
+
+print(a & b)   # intersection
+
+
+print(12%3)
+
+a=[1,2,3]
+
+b=[1,2,3]
+
+print(a==b)  # --> refers the value
+
+print(a is b)  # --> refers the memory
+
+
+b="VETRI"
+print(b[::-1])
+
+# palindrome or not
+
+name="MOM"
+
+if(name[::-1]==name):
+    print("name is palindrome")
+else:
+    print("not palindrome")
+
+for i in range(1,11):
+    print(i,"x2=",i*2)
+
+ 
+
+
+
