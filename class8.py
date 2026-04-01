24c8251e9fb105e8423827f97abb3e15652e2a72
Vetri L <vsvetri9@gmail.com>
2026-03-31 23:24:43 -0700

    Practiced Python basics

---
+++ class8.py
@@ -0,0 +1,20 @@
+players=["virat","rohit","warner"]
+print("virat" in players)
+numbers=(18,45,1)
+print(21 in numbers)
+
+# List is mutable
+
+#tuple is immutable
+
+
+team_name={ "RCB","MI","SRH"}
+print("RCB" not in team_name )
+
+number=int(input("enter number:"))
+if(number>=100 and number<=999):
+    print("Number is 3 digit")
+else:
+    print("Number is not 3 digit")
+
+
